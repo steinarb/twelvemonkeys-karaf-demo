9ef4b7d29a9256c22891a84b2d20e7a2f66cfcc3
Steinar Bang <sb@dod.no>
2023-10-08 09:58:59 +0200

    Handle IMAGE_METADATA_FAILURE in apiErrorsReducer, and remove non-existing redux actions

--- src/main/frontend/src/reducers/apiErrorsReducer.js
+++ src/main/frontend/src/reducers/apiErrorsReducer.js
@@ -1,12 +1,10 @@
 import { createReducer } from '@reduxjs/toolkit';
 import {
-    INCREMENT_FAILURE,
-    DECREMENT_FAILURE,
+    IMAGE_METADATA_FAILURE,
 } from '../reduxactions';
 
 const apiErrorsReducer = createReducer(0, {
-    [INCREMENT_FAILURE]: (state, action) => ({ ...state, increment: action.payload }),
-    [DECREMENT_FAILURE]: (state, action) => ({ ...state, decrement: action.payload }),
+    [IMAGE_METADATA_FAILURE]: (state, action) => ({ ...state, increment: action.payload }),
 });
 
 export default apiErrorsReducer;
